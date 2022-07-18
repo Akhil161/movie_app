13406ae32c579fda8d402e527ef42885f9c3d088
Akhil <akhilgarg1610@gmail.com>
2022-07-18 22:41:32 +0530

    d

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
                 //console.log(navItem[j]);
                 
             }
-            e.currentTarget.classList.add("active");
+            e.target.classList.add("active");
         });
         
     }
@@ -24,7 +24,7 @@ export default function Navbar() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" >
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Movies</Link>

2952dee6be1651b7cff00cbb3a1a802d4eaea791
Akhil <akhilgarg1610@gmail.com>
2022-07-18 22:33:40 +0530

    j

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './NavBar.css'
+
 
 export default function Navbar() {
     let navItem = document.querySelectorAll(".nav-link");
@@ -24,7 +24,7 @@ export default function Navbar() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse" >
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Movies</Link>
